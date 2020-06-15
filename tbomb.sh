3135fe1d7b526476afcc7f853151b499665d044a
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-06-15 17:29:25 +0530

    Update tbomb.sh

--- tbomb.sh
+++ tbomb.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 clear
+
+limit=100
+sleep 1
+echo " WELCOME TO YOU"
+sleep 1
+echo -n "what is your Name?:"
+read Name;
+sleep 2
+echo "Hello Mr/ Mrs [$Name]"
+sleep 1
+clear
+echo -n "Did you subscribed my youtube channel ? y/n :"
+read pilih
+if [ $pilih = "y" ];
+then
+sleep 2
+echo "Thanks for subscribing my channel"
+sleep 2
+clear
+elif [ $pilih = "n" ];
+then
+sleep 2
+echo "Please SUBSCRIBE CHANNEL My channel"
+termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag "
+sleep 4
+sleep 2
+fi
+clear
+
+
+
 echo -e "\e[4;31m shravanjerry Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m tbomb \e[0m"
